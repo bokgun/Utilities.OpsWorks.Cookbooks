7fe7df90324471784355532c2a6734d3fbd4d721
Mike Prosper <mprosper@casenex.com>
2014-04-30 16:30:32 -0700

    Added metadata.rb and changed name to free_tds

---
+++ free_tds/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "CaseNEX, LLC"
+license           "Apache 2.0"
+description       "Installs free_tds prerequisite for tiny_tds"
+version           "0.1"
+recipe            "free_tds::setup", "Installs free_tds"
+
